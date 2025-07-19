539b7e4549bce8a4d466505afce4c6859ab8394e
GOMazk <gommazuk@naver.com>
2025-07-20 00:14:36 +0900

    FOLLOW OpenLR2 commit
    309c262

--- LR2f.h
+++ LR2f.h
@@ -192,7 +192,7 @@ extern CRITICAL_SECTION DB_lock;
 int DB_EnterCriticalSection();
 int DB_LeaveCriticalSection();
 bool ANSItoUTF8(LPCSTR str, char * oBuf, size_t * oSize);
-bool UTF8toANSI(LPCSTR str, void * oBuf, size_t * oSize);
+bool UTF8toANSI(LPCSTR str, char * oBuf, size_t * oSize);
 int SQL_Run(CSTR queryStr, sqlite3 * sql);
 int SQL_prepare(CSTR queryStr, sqlite3 * sql, sqlite3_stmt ** ppStmt);
 CSTR SQL_GetColumn(int i, sqlite3_stmt * pStmt);
